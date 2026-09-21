3d1b54f50e3e065ebc92c4e351072200680f005f
Manar <manarkrid3@gmail.com>
2026-09-21 14:58:46 +0200

    fix_backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { join } from 'path';
+
 import { Module } from '@nestjs/common';
+
 import { GraphQLModule } from '@nestjs/graphql';
+
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 import { AppController } from './app.controller.js';
+
 import { AppService } from './app.service.js';
+
 import { GraphQlResolver } from './resolver.js';
+
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
@@ -15,6 +23,10 @@ import { GraphQlResolver } from './resolver.js';
         path: join(process.cwd(), 'src/graphql.ts'),
         outputAs: 'class',
       },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'public', 'icones'),
+      serveRoot: '/icones',
     }),
   ],
   controllers: [AppController],
